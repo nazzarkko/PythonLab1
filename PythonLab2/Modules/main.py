3a6e6d0341dceda3fbc4b9a6fd40d4bbf8626cde
Nazar Shiyka <nazar.shyika.ir.2022@lpnu.ua>
2023-06-04 17:44:54 +0300

    Adding fourth lab

--- PythonLab2/Modules/main.py
+++ PythonLab2/Modules/main.py
@@ -1,23 +1,23 @@
-from PythonLab2.Modules.GamingLaptop import GamingLaptop
-from PythonLab2.Modules.BussinesLaptop import BussinesLaptop
-from PythonLab2.Modules.MacBook import MacBook
-from PythonLab2.Modules.UltraBook import UltraBook
-from PythonLab2.Manager.LaptopManager import LaptopManager
+"""
+Create a collection of laptops and perform operations on it.
+"""
+from PythonLab2.Modules.gaming_laptop import GamingLaptop
+from PythonLab2.Modules.bussines_laptop import BussinesLaptop
+from PythonLab2.Modules.mac_book import MacBook
+from PythonLab2.Modules.ultra_book import UltraBook
+from PythonLab2.Manager.laptop_manager import LaptopManager
 
 if __name__ == '__main__':
     laptops = LaptopManager()
+    """
+    Create an instance of LaptopManager to manage a collection of laptops.
+    """
+
     laptops.add_laptop(GamingLaptop("Asus", 16, 15.6, 6, "Nvidia", 2))
-    laptops.add_laptop(BussinesLaptop("Lenovo", 8, 14, 8, 1.5, 1))
-    laptops.add_laptop(MacBook("MacBook", 8, 13, 10, "M1", 10))
+    laptops.add_laptop(BussinesLaptop("Lenovo", 8, 14, 8, "Windows 10 Home", "True"))
+    laptops.add_laptop(MacBook("MacBook", 8, 13, 10, "M1", "MacOS"))
     laptops.add_laptop(UltraBook("HP", 8, 13, 10, 1.5, 1))
     laptops.add_laptop(GamingLaptop("Dell", 32, 16.2, 6, "GTX", 4))
-    for laptop in laptops.laptops:
-        print(laptop)
-    print("Our laptops with choosed ram is:\n ")
-    laptops1 = laptops.choose_laptop_with_ram(8)
-    for laptop in laptops1:
-        print(laptop)
-    print("Our laptops with screen size more than choosed: \n")
-    laptops2 = laptops.find_laptop_with_more_screen_size(15.0)
-    for laptop in laptops2:
-        print(laptop)
+
+    laptops[0].replace_battery(1)
+
